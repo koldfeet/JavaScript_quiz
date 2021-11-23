a1e1a81381257c55a24198ed054926c7846be06c
koldfeet <chris.tr88@gmail.com>
2021-11-23 12:52:17 -0700

    starting css page

--- apps.js
+++ apps.js
@@ -50,35 +50,25 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
         var userAnswer = '';
         var numCorrect = 0;
         
-        // for each question...
         for(var i=0; i<questions.length; i++){
     
-            // find selected answer
             userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
             
-            // if answer is correct
             if(userAnswer===questions[i].correctAnswer){
-                // add to the number of correct answers
                 numCorrect++;
                 
-                // color the answers green
                 answerContainers[i].style.color = 'lightgreen';
             }
-            // if answer is wrong or blank
             else{
-                // color the answers red
                 answerContainers[i].style.color = 'red';
             }
         }
     
-        // show number of correct answers out of total
         resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
     }
 
-	// show the questions
 	showQuestions(questions, quizContainer);
 
-	// when user clicks submit, show results
 	submitButton.onclick = function(){
 		showResults(questions, quizContainer, resultsContainer);
 	}
